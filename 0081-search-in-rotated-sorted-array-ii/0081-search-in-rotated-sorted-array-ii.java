c3e98fbb2ea78217da347b01fd74676cea3c5528
roshan3517 <141671667+roshan3517@users.noreply.github.com>
2023-08-10 09:06:44 +0530

    Time: 0 ms (100%), Space: 43.3 MB (25.09%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,8 +1,29 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return true;
+        int start=0, end=nums.length-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target) return true;
+
+            //if thre are duplicate
+            if(nums[start]==nums[mid ] && nums[mid]==nums[end]){
+                start++;
+                end--;
+            }
+            //left half is stored
+            else if(nums[start]<=nums[mid]){
+                if(target>=nums[start] && target<=nums[mid]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }else{
+                if(target<=nums[end] && target>=nums[mid]){
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
             }
         }
         return false;
