7eb2eeab3e1c13bb8e3bf58a85c12e6c51ee5491
roshan3517 <141671667+roshan3517@users.noreply.github.com>
2023-09-10 16:26:05 +0530

    Time: 5 ms (50%), Space: 43.2 MB (50%) - LeetHub

---
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numberOfPoints(List<List<Integer>> nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(List<Integer> list:nums){
+            for(int i=list.get(0);i<=list.get(1);i++){
+                set.add(i);
+            }
+        }
+        return set.size();
+    }
+}
