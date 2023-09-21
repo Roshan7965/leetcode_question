14e991e826d013db65a3f81a0f15ab67f9bde5b7
roshan3517 <141671667+roshan3517@users.noreply.github.com>
2023-09-21 18:18:02 +0530

    Time: 1 ms (100%), Space: 42.5 MB (99.95%) - LeetHub

--- 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.java
+++ 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.java
@@ -3,21 +3,17 @@ class Solution {
         int sum=0;
         for(int i=0;i<nums.size();i++){
             int index=i;
-            int setBits=0;
-
-            //Count the number of set bits int he binary represntation of the index;
+            int setBit=0;
             while(index>0){
-                setBits += index & 1;
-                index>>=1;
-
+               setBit+=index&1;
+               index>>=1;
             }
-            //check if the count of set bits matches matches k
-            if(setBits==k){
+            if(setBit==k){
                 sum+=nums.get(i);
-
             }
 
         }
         return sum;
+        
     }
 }
