5c90e99b16732aa7ff1a63a9f26b5bd4e4ddad99
roshan3517 <141671667+roshan3517@users.noreply.github.com>
2023-09-22 20:55:57 +0530

    Time: 1 ms (89.69%), Space: 40.8 MB (18.89%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,19 +1,12 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        if((s!=null && t==null)||(s==null && t!=null) ||(s.length()>0 && t.length()==0) ){
-            return false;
+        int sStart=0;
+        int tStart=0;
+        while(sStart<s.length() && tStart<t.length()){
+            if(s.charAt(sStart)== t.charAt(tStart)) sStart++;
+            tStart++;
         }
-        if((s.length()==0 && t.length()>0) || (s.length()==0 &&t.length()==0)){
-            return true;
-        }
-        Stack<Character> stack1=new Stack();
-        for(int i=0;i<s.length();i++) stack1.push(s.charAt(i));
-        for(int i=t.length()-1;i>=0;i--){
-            if(stack1.peek()==t.charAt(i)) stack1.pop();
-            if(stack1.isEmpty()){
-                return true;
-            }
-        }
-        return false;
+        return sStart==s.length();
+        
     }
 }
