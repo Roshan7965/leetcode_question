cd7a88a633886db14ee331ef09ebc10de085a430
Roshan Tanpure <141671667+roshan3517@users.noreply.github.com>
2024-07-09 19:33:24 +0530

    Time: 0 ms (100%), Space: 40.3 MB (50.22%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int consumedBottles=0;
+        while(numBottles >=numExchange){
+            // Consume Exchange full bottles
+            consumedBottles+=numExchange;
+            numBottles-=numExchange;
+            //Exchange them for one full bottle
+            numBottles++;
+        }
+
+        // Consume the remaining numBottles(<numExchange).
+        return consumedBottles+numBottles;
+    }
+}
