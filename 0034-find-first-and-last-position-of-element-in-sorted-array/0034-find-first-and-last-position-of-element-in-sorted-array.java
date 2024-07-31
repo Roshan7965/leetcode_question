1ad5cac80ec413242da08735257b439a2deb0cd4
Roshan Tanpure <141671667+Roshan7965@users.noreply.github.com>
2024-07-31 16:15:05 +0530

    Time: 0 ms (100%), Space: 45.7 MB (79.17%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,15 +1,47 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int n=nums.length;
+        int first=firstOccuraces(nums,target);
+        if(first==-1) return new int[]{-1,-1};
+        int second=secondOccuraces(nums,target);
+        return new int[]{first,second};
+    }
+
+    public int firstOccuraces(int nums[],int target){
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
         int first=-1;
-        int second=-1;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target && first==-1){
-                first=i;
-                second=i;
-            }else if(nums[i]==target){
-                second=i;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                first=mid;
+                high=mid-1;
+            }else if(nums[mid]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
-        return new int[]{first,second};
+        return first;
+    }
+
+    public int secondOccuraces(int nums[],int target){
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        int second=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                second=mid;
+                low=mid+1;
+            }else if(nums[mid]<target){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return second;
     }
 }
