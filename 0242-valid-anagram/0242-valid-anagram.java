70fb8b45d2e97ebb20eab6af247f1c1f573fd31a
Roshan Tanpure <141671667+Roshan7965@users.noreply.github.com>
2024-08-15 22:05:00 +0530

    Time: 3 ms (88.98%), Space: 42.6 MB (94.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,15 +4,19 @@ class Solution {
             return false;
         }
         int ch1[]=new int[26];
-        int ch2[]=new int[26];
+     
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            char ch3=t.charAt(i);
-           ch1[ch-97]++;
-           ch2[ch3-97]++;
+            ch1[ch-97]++;
+           
+        }
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            ch1[ch-97]--;
+          
         }
         for(int i=0;i<26;i++){
-            if(ch1[i]!=ch2[i]){
+            if(ch1[i]!=0){
                 return false;
             }
         }
